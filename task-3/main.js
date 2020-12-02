4646134aa1f9160fe84bcf6b0940b177e62de4dd
Alexey Yalanskyy <yalanskyy@gmail.com>
2020-12-02 17:04:00 +0200

    add task3

---
+++ task-3/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let num1 = prompt("Enter number 1:");
+let num2 = prompt('Enter number 2:');
+let num3 = prompt('Enter number 3:');
+let res = +num1 + +num2 + +num3;
+
+alert('You resutl is: ' + res);
